96e01a28618e9b2153079c34b9b1b19895713193
Irene-nandy <irenekasiva75@gmail.com>
2020-09-30 00:18:56 +0300

    add html config templates changes

--- config.py
+++ config.py
@@ -1,32 +1,32 @@
-# import os
+import os
 
-# class Config:
+class Config:
 
     
-#     SECRET_KEY = '9999'
-#     #
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = '9999'
+    #
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-#     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
-#     #  email configurations
-#     MAIL_SERVER = 'smtp.googlemail.com'
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
-# class ProdConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     
 
 
-# class DevConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blog'
-#     DEBUG = True
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blog'
+    DEBUG = True
 
-# config_options = {
-# 'development':DevConfig,
-# 'production':ProdConfig
-# }
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
