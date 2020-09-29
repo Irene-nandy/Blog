dd32fbe631eb45194aef7fced66ff286bb97d7b8
Irene-nandy <irenekasiva75@gmail.com>
2020-09-30 00:09:00 +0300

    add emails changes

--- app/emails/__init__.py
+++ app/emails/__init__.py
@@ -1,3 +1,3 @@
-# from flask import Blueprint
-# email= Blueprint('email' ,__name__)
-# from . import views,forms
+from flask import Blueprint
+email= Blueprint('email' ,__name__)
+from . import views,forms
